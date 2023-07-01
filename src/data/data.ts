c62e23abc2bc45bae99dd34f3eb07dae99916e12
unknown <rrrr0ffff@gmail.com>
2023-07-01 11:30:18 -0300

    added dummy data

---
+++ src/data/data.ts
@@ -0,0 +1,12 @@
+const reports = [
+  { id: 1, type: 'income', amount: 500 },
+  { id: 2, type: 'income', amount: 600 },
+  { id: 3, type: 'outcome', amount: 690 },
+  { id: 4, type: 'outcome', amount: 450 },
+  { id: 5, type: 'income', amount: 300 },
+  { id: 6, type: 'outcome', amount: 730 },
+  { id: 7, type: 'income', amount: 550 },
+  { id: 8, type: 'outcome', amount: 900 },
+];
+
+export default reports;
--- src/reports/report.controller.ts
+++ src/reports/report.controller.ts
@@ -1,18 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import reports from 'src/data/data';
 
 @Controller()
 export class ReportController {
   @Get()
   getReports() {
-    return [
-      { id: 1, type: 'income', amount: 500 },
-      { id: 2, type: 'income', amount: 600 },
-      { id: 3, type: 'outcome', amount: 690 },
-      { id: 4, type: 'outcome', amount: 450 },
-      { id: 5, type: 'income', amount: 300 },
-      { id: 6, type: 'outcome', amount: 730 },
-      { id: 7, type: 'income', amount: 550 },
-      { id: 8, type: 'outcome', amount: 900 },
-    ];
+    return reports;
   }
 }
