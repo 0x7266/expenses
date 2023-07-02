a5d98a7ef52b45f27509729b56b697435f9fc0e7
unknown <rrrr0ffff@gmail.com>
2023-07-02 00:53:19 -0300

    typed the dummy data

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,12 +1,82 @@
-const reports = [
-  { id: 1, type: 'income', amount: 500 },
-  { id: 2, type: 'income', amount: 600 },
-  { id: 3, type: 'outcome', amount: 690 },
-  { id: 4, type: 'outcome', amount: 450 },
-  { id: 5, type: 'income', amount: 300 },
-  { id: 6, type: 'outcome', amount: 730 },
-  { id: 7, type: 'income', amount: 550 },
-  { id: 8, type: 'outcome', amount: 900 },
+export enum ReportType {
+  INCOME = 'income',
+  OUTCOME = 'outcome',
+}
+
+type Report = {
+  id: number;
+  type: ReportType;
+  amount: number;
+  source: string;
+  created_at: Date;
+  updated_at: Date;
+};
+
+const reports: Report[] = [
+  {
+    id: 1,
+    type: ReportType.INCOME,
+    source: 'Salary',
+    amount: 3800,
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id: 2,
+    type: ReportType.INCOME,
+    source: 'YouTube',
+    amount: 600,
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id: 3,
+    type: ReportType.OUTCOME,
+    source: 'Food',
+    amount: 690,
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id: 4,
+    type: ReportType.OUTCOME,
+    source: 'Internet',
+    amount: 50,
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id: 5,
+    type: ReportType.INCOME,
+    source: 'TikTok',
+    amount: 300,
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id: 6,
+    type: ReportType.OUTCOME,
+    source: 'Medicines',
+    amount: 169,
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id: 7,
+    type: ReportType.INCOME,
+    source: 'Freelancing',
+    amount: 550,
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id: 8,
+    type: ReportType.OUTCOME,
+    source: 'Vet',
+    amount: 900,
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
 ];
 
 export default reports;
